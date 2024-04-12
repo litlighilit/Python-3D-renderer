7bbc61f23f5bd240a99ab8e5e631f1ed6fe48e4f
litlighilit <litlighilit@foxmail.com>
2024-04-12 17:37:02 +0800

    do not try to set if xclip is not available

--- access_clipboard.py
+++ access_clipboard.py
@@ -8,12 +8,17 @@ else:
         ret = subprocess.run(['which', cmd], stdout=subprocess.PIPE)
         return ret.returncode == 0
     _CLIP_CMD = 'xclip'
+    _HAS_CLIP = False
     if not is_command_available(_CLIP_CMD):
         import warnings
         warnings.warn(f'{_CLIP_CMD} is not available, \
                     please install it to support set_clipboard')
+    else:
+        _HAS_CLIP = True
+
     def set_clipboard(s):
-        p = subprocess.Popen([_CLIP_CMD], stdin=subprocess.PIPE, text=True)
-        p.stdin.write(s)
-        p.flush()
+        if _HAS_CLIP:
+            p = subprocess.Popen([_CLIP_CMD], stdin=subprocess.PIPE, text=True)
+            p.stdin.write(s)
+            p.flush()
 
